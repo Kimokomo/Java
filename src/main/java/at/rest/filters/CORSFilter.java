b5d5d3f160b68f3b2842faf023e420161554e512
kimokomo <karim.yassin@hotmail.com>
2025-06-08 16:59:10 +0100

    Load CORS allowed origin from environment-specific properties file

--- src/main/java/at/rest/filters/CORSFilter.java
+++ src/main/java/at/rest/filters/CORSFilter.java
@@ -5,16 +5,40 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+
+    private static final Logger logger = Logger.getLogger(CORSFilter.class.getName());
+
+    private static final String DEFAULT_ENV = "dev";
+    private static final Properties props = new Properties();
+
+    static {
+        String env = System.getProperty("app.env", DEFAULT_ENV);
+        logger.info("CORS ENV beim Start: " + env);
+        try (InputStream input = CORSFilter.class.getClassLoader().getResourceAsStream("cors-" + env + ".properties")) {
+            if (input != null) {
+                props.load(input);
+            } else {
+                throw new RuntimeException("config file cors-" + env + ".properties nicht gefunden!!!!!");
+            }
+        } catch (IOException e) {
+            logger.warning("Fehler beim Laden der CORS-Konfiguration");
+            throw new RuntimeException("Failed to load CORS config", e);
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
 
-        //http://localhost:4200
-        //*
-        //https://slicy.it.com
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        String allowedOrigin = props.getProperty("allowed.origin", "*");
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", allowedOrigin);
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
